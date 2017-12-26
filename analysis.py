b78d21c91f19852d370da70adb0339b00274a80e
Callum Iddon <callum.iddon@gmail.com>
2017-12-26 11:50:04 +0000

    Remove hardcoded file names

--- analysis.py
+++ analysis.py
@@ -5,13 +5,9 @@ import os
 
 os.chdir("C:\\Users\\Callum\\Dropbox\\code\\Polar-H7-Pi")
 
-names = ["11-09-37.237962.csv", "11-11-18.485575.csv", "11-12-58.732744.csv",
-         "11-14-40.477777.csv"]
-print(os.getcwd())
+names = [f for f in os.listdir() if f[-4:] == ".csv"]
 
 df = pd.concat(pd.read_csv(name, names=["HR", "Time"]) for name in names)
-
-
 
 df["Time"] = pd.to_datetime("26/12/2017 " + df["Time"],
                             infer_datetime_format=True)
